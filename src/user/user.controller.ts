28d1e1c8f9d3653f75aa9af795acc90c2ddb6d76
vinicius <viniciusfernandesoliveira8@gmail.com>
2025-11-25 21:28:57 -0300

    feat: add route, functions and dto to handle with update of user

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -3,6 +3,7 @@ import {
   Controller,
   Get,
   Param,
+  Patch,
   Post,
   Req,
   UseGuards,
@@ -13,6 +14,7 @@ import { CreateUserDto } from './dto/create-user.dto';
 import { UserService } from './user.service';
 import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
 import type { AuthenticatedRequest } from 'src/auth/types/authenticated-request';
+import { UpdateUserDto } from './dto/update-user.dto';
 
 @Controller('user')
 export class UserController {
@@ -33,5 +35,11 @@ export class UserController {
   @Post()
   create(@Body() dto: CreateUserDto) {
     return this.userService.create(dto);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Patch('me')
+  update(@Req() req: AuthenticatedRequest, dto: UpdateUserDto) {
+    return this.userService.update(req.user.id, dto);
   }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,9 +1,15 @@
-import { ConflictException, Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  ConflictException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { Repository } from 'typeorm';
 import { User } from './entities/user.entity';
 import { InjectRepository } from '@nestjs/typeorm';
 import { CreateUserDto } from './dto/create-user.dto';
 import { HashingService } from 'src/common/hashing/hashing.service';
+import { UpdateUserDto } from './dto/update-user.dto';
 
 @Injectable()
 export class UserService {
@@ -13,16 +19,18 @@ export class UserService {
     private readonly hashingService: HashingService,
   ) {}
 
-  async create(dto: CreateUserDto) {
-    const exists = await this.userRepository.exists({
-      where: {
-        email: dto.email,
-      },
+  async validateIfEmailExists(email: string) {
+    const exists = await this.userRepository.findOneBy({
+      email,
     });
 
     if (exists) {
       throw new ConflictException('E-mail já existe');
     }
+  }
+
+  async create(dto: CreateUserDto) {
+    await this.validateIfEmailExists(dto.email);
 
     const hashedPassword = await this.hashingService.hash(dto.password);
     const newUser: CreateUserDto = this.userRepository.create({
@@ -43,7 +51,35 @@ export class UserService {
     return this.userRepository.findOneBy({ id });
   }
 
+  async findOneByOrFail(userData: Partial<User>) {
+    const user = await this.userRepository.findOneBy(userData);
+
+    if (!user) {
+      throw new NotFoundException('Usuário não encontrado');
+    }
+
+    return user;
+  }
+
   save(user: User) {
     return this.userRepository.save(user);
+  }
+
+  async update(id: string, dto: UpdateUserDto) {
+    if (!dto.name || !dto.email) {
+      throw new BadRequestException('Dados não enviados!');
+    }
+
+    const user = await this.findOneByOrFail({ id });
+
+    user.name = dto.name ?? user.name;
+
+    if (dto.email && dto.email !== user.email) {
+      await this.validateIfEmailExists(dto.email);
+      user.email = dto.email;
+      user.forceLogout = true;
+    }
+
+    return this.save(user);
   }
 }
