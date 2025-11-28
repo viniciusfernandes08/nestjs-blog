57d792ad60125a95ec0ab050e2ab0be04c056987
vinicius <viniciusfernandesoliveira8@gmail.com>
2025-11-27 22:04:42 -0300

    feat: add post entity

--- src/post/entities/post.entity.ts
+++ src/post/entities/post.entity.ts
@@ -1,1 +1,37 @@
-export class Post {}
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity()
+export class Post {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  title: string;
+
+  @Column({ unique: true })
+  slug: string;
+
+  @Column('text')
+  content: string;
+
+  @Column()
+  excerpt: string;
+
+  @Column({ nullable: true })
+  coverImageUrl: string;
+
+  @Column({ default: false })
+  published: boolean;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+}
